d0b987b034f1431bfe67d0217e17c8d98c6740cb
MaoScut <849345372@qq.com>
2017-09-09 17:29:51 +0800

    动态规划求最长公共子序列

---
+++ codewars/longestCommonSequence.js
@@ -0,0 +1,33 @@
+function lcs(x, y) {
+  const lenx = x.length;
+  const leny = y.length;
+  const cache = new Array(lenx);
+  for (let i = -1; i < lenx; i += 1) {
+    cache[i] = [];
+    cache[i][-1] = '';
+  }
+  for (let j = -1; j < leny; j += 1) {
+    cache[-1][j] = '';
+  }
+  let result = '';
+  // 注意左上角的要先知道
+  for (let i = 0; i < lenx; i += 1) {
+    for (let j = 0; j < leny; j += 1) {
+      if (x[i] === y[j]) {
+        cache[i][j] = `${cache[i - 1][j - 1]}${x[i]}`;
+      } else {
+        cache[i][j] = cache[i - 1][j].length > cache[i][j - 1].length ?
+          cache[i - 1][j] : cache[i][j - 1];
+      }
+      if (cache[i][j].length > result.length) {
+        result = cache[i][j];
+      }
+    }
+  }
+  return result;
+}
+const str11 = 'abcdefg';
+const str12 = 'abc';
+const str21 = 'abcdefghijklmnopq';
+const str22 = 'apcdefghijklmnobq';
+console.log(lcs(str21, str22));
